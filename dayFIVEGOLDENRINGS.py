f23a9cfb1522891a4d120d0c9f65114fee9d6116
Kevin Sheng <kevinsheng2005@gmail.com>
2019-12-05 08:09:29 -0800

    asdf

--- dayFIVEGOLDENRINGS.py
+++ dayFIVEGOLDENRINGS.py
@@ -10,9 +10,10 @@ for v, i in enumerate(Data):
     if v >= waitUntilVal:
         if i in [1,2,3,4,99]: #standard protocol
             if i == 3:
-                Data[Data[v+1]] == int(input('PUT 1 U DUMB BUTT '))
+                Data[Data[v+1]] = int(input('PUT 1 U DUMB BUTT '))
                 waitUntilVal += 2
             elif i == 1:
+                #print(Data[Data[v+1]], Data[Data[v+2]])
                 Data[Data[v + 3]] = Data[Data[v+1]] + Data[Data[v+2]]
                 waitUntilVal += 4
             elif i == 2:
@@ -23,7 +24,7 @@ for v, i in enumerate(Data):
                 assert Data[Data[v+1]] == 0
                 waitUntilVal += 2
         elif str(i)[-2:] in ['01', '02', '03', '04', '99']: #0 is position, 1 is immediate
-            saved = str(i)[:2]
+            saved = str(i)[-2:]
             if str(i)[-2:] == '99':
                 break
             elif str(i)[-2:] in ['01', '02']:
@@ -41,12 +42,12 @@ for v, i in enumerate(Data):
                         argList.append(Data[v+a+1])
                 #print(argList)
                 if saved == '01':
-                    Data[argList[2]] = sum(argList[:2])
+                    Data[argList[2]] = argList[0] + argList[1]
                 else:
                     Data[argList[2]] = argList[0] * argList[1]
                 #print(i)
             else:
-                i = [int(x) for x in list(f'{i:3}')]
+                i = [int(x) for x in list(f'{i:3}')] #if the opcode was 3 or 4
                 if i[-1] == 3:
                     Data[Data[v+1]] = int(input('why does this happen'))
                 else:
@@ -56,4 +57,5 @@ for v, i in enumerate(Data):
                     else:
                         print(Data[v+1])
                         assert Data[v+1] == 0
-print(Data)
+    print(Data[:v+1], len(Data[:v+1]))
+#print(Data)

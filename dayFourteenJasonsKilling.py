f6406fd02809bfc3503b1045aa232005df711909
Kevin Sheng <kevinsheng2005@gmail.com>
2019-12-14 12:38:28 -0800

    Update dayFourteenJasonsKilling.py

--- dayFourteenJasonsKilling.py
+++ dayFourteenJasonsKilling.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 data = open('data stuff/test.txt')
 conversion = [] #each element: [product, reactants]
 with data as Data:
@@ -14,31 +16,41 @@ with data as Data:
 
 print(conversion)
 def findRawReactants(reactionList, element = 'FUEL', alreadyHave = 0, amt = 1):
-    """break everything down into ore components"""
+    """break everything down into ore components
+    amt is the amt produced, and alreadyHave is kinda misleading:
+    its just if any components (not of ores)"""
     oreCount = 0
-    leftOverOre = 0
+    alreadyHave = 0
     for v, reaction in enumerate(reactionList):
         if reaction[0][1] == element:
             increment = reaction[0][0]
             #print(increment)
             if reaction[1][1] == 'ORE': #its only ore when its used
-                production = amt//reaction[1][0] + 1
+                production = ceil(amt/reaction[1][0])
+                produced = 0
+                print(amt, reaction[1])
                 for i in range(production):
                     while reaction[1][0] <= alreadyHave:
                         alreadyHave -= reaction[1][0]
+                        produced += reaction[1][0]
                     else:
-                        oreCount += reaction[1][0]                                                                      
+                        if i == production - 1:
+                            print(produced)
+                            pass
+                        else:
+                            oreCount += reaction[1][0]
+                                                                                 
                 #print('current orecount',oreCount)
                 return increment, oreCount, alreadyHave
             else:
                 print(reaction)
                 for el in reaction[1:]:
-                    x = findRawReactants(reactionList[:v] + reactionList[v + 1:], el[1], alreadyHave)
+                    x = findRawReactants(reactionList[:v] + reactionList[v + 1:], el[1], alreadyHave, el[0])
                     oreCount += x[1]
-                    print('asdf',x)
+                    print('findRawReactants %s for %s' % (x, el))
                     print('Orecount for element %s: %s but it left over %s' %(el, x[1], x[2]))
-                    leftOverOre = x[2]
-                    print('storage: %s' % leftOverOre)
-    return increment, oreCount, oreCount - leftOverOre
+                    alreadyHave = x[2]
+                    print('storage: %s' % alreadyHave)
+    return increment, oreCount, alreadyHave
 
-print('horrible output ->',findRawReactants(conversion, 'C'))
+print('horrible output ->',findRawReactants(conversion, 'A', amt = 7))

e1d225ddc5135cec8ac086801523622aca58a4bc
Kevin Sheng <kevinsheng2005@gmail.com>
2019-12-14 17:25:08 -0800

    d

--- dayFourteenJasonsKilling.py
+++ dayFourteenJasonsKilling.py
@@ -15,42 +15,18 @@ with data as Data:
         conversion.append(temp)
 
 print(conversion)
+elements = [k[1] for d in conversion for k in d]
+elements = list(set(elements))
+leftovers = {s: 0 for s in elements}
+print(leftovers)
+
 def findRawReactants(reactionList, element = 'FUEL', alreadyHave = 0, amt = 1):
     """break everything down into ore components
     amt is the amt produced, and alreadyHave is kinda misleading:
     its just if any components (not of ores)"""
     oreCount = 0
-    alreadyHave = 0
     for v, reaction in enumerate(reactionList):
-        if reaction[0][1] == element:
-            increment = reaction[0][0]
-            #print(increment)
-            if reaction[1][1] == 'ORE': #its only ore when its used
-                production = ceil(amt/reaction[1][0])
-                produced = 0
-                print(amt, reaction[1])
-                for i in range(production):
-                    while reaction[1][0] <= alreadyHave:
-                        alreadyHave -= reaction[1][0]
-                        produced += reaction[1][0]
-                    else:
-                        if i == production - 1:
-                            print(produced)
-                            pass
-                        else:
-                            oreCount += reaction[1][0]
-                                                                                 
-                #print('current orecount',oreCount)
-                return increment, oreCount, alreadyHave
-            else:
-                print(reaction)
-                for el in reaction[1:]:
-                    x = findRawReactants(reactionList[:v] + reactionList[v + 1:], el[1], alreadyHave, el[0])
-                    oreCount += x[1]
-                    print('findRawReactants %s for %s' % (x, el))
-                    print('Orecount for element %s: %s but it left over %s' %(el, x[1], x[2]))
-                    alreadyHave = x[2]
-                    print('storage: %s' % alreadyHave)
-    return increment, oreCount, alreadyHave
+        if reaction[0][-1] == element:
 
-print('horrible output ->',findRawReactants(conversion, 'A', amt = 7))
+            pass
+    return increment, oreCount, alreadyHave

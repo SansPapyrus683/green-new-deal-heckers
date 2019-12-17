e89e4dba6c209111398197376c33b9af851ecd69
Kevin Sheng <kevinsheng2005@gmail.com>
2019-12-16 16:01:28 -0800

    Update dayFourteenJasonsKilling.py

--- dayFourteenJasonsKilling.py
+++ dayFourteenJasonsKilling.py
@@ -1,7 +1,7 @@
 from math import ceil
 from sys import exit
 
-data = open("data stuff/jason.txt")
+data = open("data stuff/test.txt")
 conversion = []  # each element: [product, reactants]
 with data as Data:
     for line in data.readlines():
@@ -20,7 +20,7 @@ with data as Data:
     print(conversion)
 
 
-def findRawReactants(reactionList, element="FUEL", amt=1):
+def findRawReactants(reactionList = conversion, element="FUEL", amt=1):
     """break everything down into ore components
     amt is the amt that we want produced
     oh and primitive elements are ones that can be directly
@@ -69,13 +69,20 @@ def findRawReactants(reactionList, element="FUEL", amt=1):
 print(findRawReactants(conversion, element="FUEL", amt=1)[1])
 
 #PART 2
-leftovers = {s: 0 for s in elements}
-totalOre = 10 ** 12
-fuelCount = 0
-while True:
-    fuelCount += 1 
-    reference = findRawReactants(conversion)
-    totalOre -= reference[1]
-    if totalOre < 0:
-        fuelCount  -= 1
-        break
+oreNum = 10**12
+#1 trillion is 12 0's
+currNum = oreNum//2
+searchRange = [1, oreNum]
+while searchRange[1] - searchRange[0] > 1:
+    leftovers = {s: 0 for s in elements}
+    if findRawReactants(amt = currNum)[1] < oreNum: #GIMME MORE
+        searchRange[0] += (searchRange[1] - searchRange[0] + 1)//2
+        currNum += (searchRange[1] - searchRange[0] + 1)//2
+    elif findRawReactants(amt = currNum)[1] > oreNum: #ooh thats a diddly darn too much
+        searchRange[1] -= (searchRange[1] - searchRange[0] + 1)//2
+        currNum -= (searchRange[1] - searchRange[0] + 1)//2
+    else:
+        print('ok we can make this much ore %s' % currNum)
+        exit()
+
+print('well we have this, so... %s' % searchRange[0])

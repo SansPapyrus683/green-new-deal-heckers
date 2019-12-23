a4fbd37a7a82de5fd2a1d84f19efac4a0affb80a
Kevin Sheng <kevinsheng2005@gmail.com>
2019-12-22 20:05:22 -0800

    d16
    i did d16 part 1, but it's really slow and ill probably tweak it later

--- bad_solutions/daySixteenSantasRobbing.py
+++ bad_solutions/daySixteenSantasRobbing.py
@@ -1,4 +1,4 @@
-file = open("data stuff/test.txt")
+file = open("data stuff/bankInNovember")
 sigCode = []
 with file as data:
     for l in data.readlines():
@@ -6,5 +6,30 @@ with file as data:
             sigCode.append(int(s))
 
 # PART 1
-print(sigCode)
+#print(sigCode)
 pattern = [0, 1, 0, -1]
+
+def oneStep(code, times):
+    for y in range(times):
+        result = []
+        for j in range(len(code)):
+            nextPatterns = []
+            for i in pattern:
+                nextPatterns.extend([i for x in range(j + 1)])
+            actualMatch = nextPatterns[1:]
+            while len(actualMatch) < len(code):
+                actualMatch.extend(nextPatterns)
+            #print(actualMatch)
+            tempSum = 0
+            for v, x in enumerate(code):
+                tempSum += x* actualMatch[v]
+            result.append(int(str(tempSum)[-1]))
+        code = result[:]
+        #print(code)   
+    return code         
+
+print(oneStep(sigCode, 100))
+
+#PART 2
+sigCode = sigCode * 10000
+print(oneStep(sigCode, 100))

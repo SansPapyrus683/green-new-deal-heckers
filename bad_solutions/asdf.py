44cbe48c3aa39f90a420b379a8e7f98eeda67761
Kevin Sheng <kevinsheng2005@gmail.com>
2019-12-27 10:26:37 -0800

    s

--- bad_solutions/asdf.py
+++
@@ -1,4 +0,0 @@
-from justStupidIntcode import *
-
-print(ascii("A,B,A,B,C,C,B,A,B,C\n"))
-print(len("A,B,A,B,C,C,B,A,B,C\n"))
--- bad_solutions/dayEighteenFansBlowing.py
+++ bad_solutions/dayEighteenFansBlowing.py
@@ -1,6 +1,40 @@
 """neptune just reminds me of spongebob
 WHO LIVES IN A PINEAPPLE UNDER THE SEA
 maybe numpy would be good but heck that"""
+from queue import Queue
+
+openPts = []  # itll be a list of tuples
+ptsWithDoors = []
+keyLoc = {}
+assoDoorLoc = {}
+
+inFan = open("data stuff/test.txt")
+with inFan as stuff:
+    yVal = 0
+    for l in reversed(stuff.readlines()):
+        xVal = 0
+        l = l.rstrip()
+        for c in list(l):
+            if c == "#":
+                xVal += 1
+            elif c == ".":
+                openPts.append((xVal, yVal))
+                ptsWithDoors.append((xVal, yVal))
+            elif c == "@":
+                openPts.append((xVal, yVal))
+                ptsWithDoors.append((xVal, yVal))
+                currPos = (xVal, yVal)
+            else:
+                if c.upper() == c:  # door found
+                    assoDoorLoc[c] = (xVal, yVal)
+                    ptsWithDoors.append((xVal, yVal))
+                else:  # key instead
+                    keyLoc[c] = (xVal, yVal)
+                    openPts.append((xVal, yVal))
+                    ptsWithDoors.append((xVal, yVal))
+            xVal += 1
+        yVal += 1
+
 
 def findNeighbors(pt: "point", ptList: "list of good points") -> "list of neighbors":
     possibleNeighbors = [
@@ -15,77 +49,64 @@ def findNeighbors(pt: "point", ptList: "list of good points") -> "list of neighb
             goodNeighbors.append(p)
     return goodNeighbors
 
-def goToPos(curr: "coordinate", ptList, goal) -> "amt of movement needed":
-    moveCount = 0
-    frontier = [curr]  # or toBeProcessed
-    visited = [curr]
-    while frontier:  # while there still is space
+
+def goToPos(start, ptList, goal):
+    #path that goes to the thing along with the amt of moves needed
+    frontier = Queue()
+    frontier.put(start)
+    camefrom = {}
+    camefrom[start] = None
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in findNeighbors(current, ptList):
+            if next not in camefrom:
+                frontier.put(next)
+                camefrom[next] = current
+
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = camefrom[current]
+
+    return len(path), path
+
+
+def findAvailable(fromPos=currPos):
+    frontier = [fromPos]
+    visited = [fromPos]
+    available = []
+    while frontier:
         inLine = []
         for pt in frontier:
-            goodPts = [p for p in ptList if p not in visited]
-            visited.extend(findNeighbors(pt, goodPts))
-            inLine.extend(findNeighbors(pt, goodPts))
-        moveCount += 1
+            for pt in [p for p in findNeighbors(pt, openPts) if p not in visited]:
+                visited.append(pt)
+                inLine.append(pt)
         frontier = inLine
-        if goal in frontier:
-            return moveCount
-    return 0  # you just cant go there.
-
-def keyEval():
-    keyDistanceDic = {}  # order matters- key a and key b both unblock different paths
-    for v, k in enumerate(keyLoc):
-        forThisKey = {}
-        goodPts = placesToGo[:]
-        for d in assoDoorLoc:
-            if d[-1].lower() == k[-1]:
-                goodPts.append(d[:-1])
-
-        for ke in keyLoc[:v] + keyLoc[v+1:]:
-            if goToPos(k[:-1], goodPts, ke[:-1]):
-                forThisKey[ke] = goToPos(k[:-1], goodPts, ke[:-1])
-                continue
-            else:
-                forThisKey[ke] = 0 #you cant go from this key to that key
-        keyDistanceDic[k[-1]] = forThisKey
-    return keyDistanceDic
-
-def findAvailable():
-    available = {}
-    for k in keyLoc:
-        if goToPos(currPos, placesToGo, k[:-1]):
-            available[k[-1]] = goToPos(currPos, placesToGo, k[:-1])
+        for key in keyLoc:
+            if keyLoc[key] in frontier:
+                available.append(key)
 
     return available
 
-placesToGo = []  # itll be a list of tuples
-keyLoc = []
-assoDoorLoc = []
-
-inFan = open("data stuff/test.txt")
-with inFan as stuff:
-    yVal = 0
-    for l in reversed(stuff.readlines()):
-        xVal = 0
-        l = l.rstrip()
-        for c in list(l):
-            if c == "#":
-                xVal += 1
-            elif c == ".":
-                placesToGo.append((xVal, yVal))
-            elif c == "@":
-                placesToGo.append((xVal, yVal))
-                currPos = (xVal, yVal)
-            else:
-                if c.upper() == c:  # door found
-                    assoDoorLoc.append(
-                        (xVal, yVal, c)
-                    )  # itll be added to placesToGo later
-                else:  # key instead
-                    keyLoc.append((xVal, yVal, c))
-                    placesToGo.append((xVal, yVal))
-            xVal += 1
-        yVal += 1
 
 # PART 1 OMG THIS IS WAY TOO LONG
-print(keyEval())
-print(findAvailable())
+available = findAvailable()
+neededKeys = {} #for each key of the dictionary, you need the values(empty if none)
+
+for k in keyLoc:
+    #print(goToPos(keyLoc[k], ptsWithDoors, currPos))
+    keyPath = goToPos(keyLoc[k], ptsWithDoors, currPos)[1]
+    keyList = []
+    for d in assoDoorLoc:
+        if assoDoorLoc[d] in keyPath:
+            keyList.append(d.lower())
+
+    neededKeys[k] = keyList
+
+print(neededKeys)

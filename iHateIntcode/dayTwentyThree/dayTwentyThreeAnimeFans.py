afb9c8ec19b591da1ad0b8741d93f07cd0cdcedc
Kevin Sheng <kevinsheng2005@gmail.com>
2020-01-14 15:26:11 -0800

    sync

--- iHateIntcode/dayTwentyThree/dayTwentyThreeAnimeFans.py
+++ iHateIntcode/dayTwentyThree/dayTwentyThreeAnimeFans.py
@@ -1,4 +1,25 @@
 from iHateIntcode.justStupidIntcode import *
+from queue import Queue
+
 
 class CategorySix(intCode):
-    pass
+    def __init__(self, codeIn, address):
+        super().__init__(codeIn)
+        self.inputQueue = Queue()
+        self.networkAddress = address
+
+    def opThree(self, arg1):
+        self.v += 2
+
+    def opFour(self, arg1):
+        self.v += 2
+
+    def __str__(self):
+        print('computer at address %i' % self.networkAddress)
+
+with open('animeIsTrash.txt') as code:
+    data = [int(i) for i in code.read().rstrip().split(sep=',')]
+    computerList = [CategorySix(data, x) for x in range(50)]
+
+for comp in computerList:
+    print(comp)

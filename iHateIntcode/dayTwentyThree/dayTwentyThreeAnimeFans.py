50d41b45df297a2533dd2ce637712f188fe21b29
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-01-16 07:32:27 -0800

    sync

--- iHateIntcode/dayTwentyThree/dayTwentyThreeAnimeFans.py
+++ iHateIntcode/dayTwentyThree/dayTwentyThreeAnimeFans.py
@@ -1,25 +1,29 @@
 from iHateIntcode.justStupidIntcode import *
 from queue import Queue
 
-
 class CategorySix(intCode):
     def __init__(self, codeIn, address):
         super().__init__(codeIn)
         self.inputQueue = Queue()
         self.networkAddress = address
+        self.firstRun = True
+        self.outputs = []
 
     def opThree(self, arg1):
+        if self.firstRun:
+            self.data[arg1] = self.networkAddress
         self.v += 2
 
     def opFour(self, arg1):
         self.v += 2
 
     def __str__(self):
-        print('computer at address %i' % self.networkAddress)
+        return f'computer at address %i' % self.networkAddress
 
-with open('animeIsTrash.txt') as code:
+with open('test.txt') as code:
     data = [int(i) for i in code.read().rstrip().split(sep=',')]
-    computerList = [CategorySix(data, x) for x in range(50)]
+    computerList = [CategorySix(data, x) for x in range(2)]
 
+totalOutputs = []
 for comp in computerList:
     print(comp)

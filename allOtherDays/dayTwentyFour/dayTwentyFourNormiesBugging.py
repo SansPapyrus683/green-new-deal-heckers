ee578d1e671de69c7b6657cceebbdb345ebf59ab
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-01-22 10:24:22 -0800

    Update dayTwentyFourNormiesBugging.py

--- allOtherDays/dayTwentyFour/dayTwentyFourNormiesBugging.py
+++ allOtherDays/dayTwentyFour/dayTwentyFourNormiesBugging.py
@@ -1,26 +1,38 @@
 from dumbMazes.iHateMazes import findNeighbors
 
-totalCoo = []
-cooCheck = set()
+totalCoo = {}
 
 with open('redditGood.txt') as bugs:
     yVal = 0
     for line in bugs.readlines():
         xVal = 0
         for c in line.rstrip():
-            totalCoo.append([xVal, yVal, c == '#'])
+            totalCoo[(xVal, yVal)] = c == '#'
             xVal += 1
         yVal += 1
+borderValues = (max(p[0] for p in totalCoo),
+                max(p[1] for p in totalCoo),
+                min(p[0] for p in totalCoo),   # this is the values of all the border points or smth like that
+                min(p[1] for p in totalCoo))
 
-for coo in totalCoo:
-    allNeighbors = [[coo[0], coo[1]],
-                    [coo[0], coo[1] + 1],
-                    [coo[0] + 1, coo[1]],
-                    [coo[0] - 1, coo[1]],
-                    [coo[0], coo[1] - 1]]
-    for p in allNeighbors:
-        cooCheck.add(tuple(p))
 
-for i in range(3):
+def borderCheck(pt):
+    if pt[0] in borderValues or pt[1] in borderValues:
+        return True
+    return False
+
+
+for i in range(1):
     for coo in totalCoo:
-        pass
+        neighborBugCount = 0
+        if borderCheck(coo):
+            pass  # do smth for the pts on the border
+        else:
+            neighbors = ([coo[0] + 1, coo[1]],
+                         [coo[0] - 1, coo[1]],
+                         [coo[0], coo[1] - 1],
+                         [coo[0], coo[1] + 1])
+            for pt in neighbors:
+                if totalCoo[pt]:
+                    neighborBugCount += 1
+                    

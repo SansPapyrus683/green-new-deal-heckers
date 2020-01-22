0e175e9363c2fd104b642af9d2cf3b825ccbc3bf
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-01-22 07:57:55 -0800

    Update dayTwentyFourNormiesBugging.py

--- allOtherDays/dayTwentyFour/dayTwentyFourNormiesBugging.py
+++ allOtherDays/dayTwentyFour/dayTwentyFourNormiesBugging.py
@@ -1,7 +1,7 @@
 from dumbMazes.iHateMazes import findNeighbors
 
 totalCoo = []
-cooCheck = {}
+cooCheck = set()
 
 with open('redditGood.txt') as bugs:
     yVal = 0
@@ -13,9 +13,13 @@ with open('redditGood.txt') as bugs:
         yVal += 1
 
 for coo in totalCoo:
-    if not len(findNeighbors(tuple(coo[:-1]), {tuple(c[:-1]) for c in totalCoo})) == 4:
-        print(coo)
-        pass  # extend the cooCheck by some stuff
+    allNeighbors = [[coo[0], coo[1]],
+                    [coo[0], coo[1] + 1],
+                    [coo[0] + 1, coo[1]],
+                    [coo[0] - 1, coo[1]],
+                    [coo[0], coo[1] - 1]]
+    for p in allNeighbors:
+        cooCheck.add(tuple(p))
 
 for i in range(3):
     for coo in totalCoo:

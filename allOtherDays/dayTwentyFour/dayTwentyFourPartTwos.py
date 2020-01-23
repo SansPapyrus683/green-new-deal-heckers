f880e77a0fbd9b749eeab77ca3f3a32bb8842f45
Kevin Sheng <kevinsheng2005@gmail.com>
2020-01-23 09:06:36 -0800

    d24 p2
    kinda slow, but at least it works

--- allOtherDays/dayTwentyFour/dayTwentyFourPartTwos.py
+++ allOtherDays/dayTwentyFour/dayTwentyFourPartTwos.py
@@ -1,9 +1,9 @@
 from dumbMazes.iHateMazes import findNeighbors
-from collections import defaultdict
+from sys import exit
 
 # this is the part 2 file
 
-totalCoo = defaultdict(False)
+totalCoo = {}
 generalStruct = set()
 with open('redditGood.txt') as bugs:
     yVal = 0
@@ -12,19 +12,17 @@ with open('redditGood.txt') as bugs:
         for c in line.rstrip():
             generalStruct.add((xVal, yVal))  # we'll need the (2, 2) later- don't worry
             if xVal == 2 and yVal == 2:
+                xVal += 1
                 continue
             totalCoo[(xVal, yVal, 0)] = c == '#'
             xVal += 1
         yVal += 1
 
-print(totalCoo)
-print(generalStruct)
-
 
 def erisNeighbors(cell: tuple):
     """each cell goes like (xValInCurrLevel, yValInCurrLevel, actualLevel(zCoo basically))
     it works w. the constraints in the problem, so it isn't general"""
-    neighbors = {pt + (cell[-1],) for pt in findNeighbors(cell[:-1], generalStruct)}
+    neighbors = {point + (cell[-1],) for point in findNeighbors(cell[:-1], generalStruct)}
     if (2, 2) in findNeighbors(cell[:-1], generalStruct):  # ok this one has 8, level goes +1
         neighbors.remove((2, 2, cell[-1]))
         if cell[:-1] == (2, 1):  # top
@@ -35,17 +33,44 @@ def erisNeighbors(cell: tuple):
             neighbors.update({(0, n, cell[-1] + 1) for n in range(5)})
         elif cell[:-1] == (2, 3):  # bottom
             neighbors.update({(n, 4, cell[-1] + 1) for n in range(5)})
-    else:  # ok this one is one the outside, level goes -1
+    elif 0 in cell[:-1] or 4 in cell[:-1]:  # ok this one is one the outside, level goes -1
         if cell[0] == 0:  # all separate to account for corners
             neighbors.update({(1, 2, cell[-1] - 1)})
-        if cell[0] == 5:
-            neighbors.update({(3, 2, cell[-1] - 1)})
-        if cell[1] == 0:
+        if cell[0] == 4:
+            neighbors.update({(3, 2, cell[-1] - 1)})w
             neighbors.update({(2, 1, cell[-1] - 1)})
-        if cell[1] == 5:
+        if cell[1] == 4:
             neighbors.update({(2, 3, cell[-1] - 1)})
     return neighbors
 
 
 for i in range(200):
-    pass
+    expandGrid = {}
+    for coo in totalCoo:
+        expandGrid.update({p: False for p in erisNeighbors(coo)})
+    for p in expandGrid:
+        if p not in totalCoo:
+            totalCoo[p] = False
+
+    newGrid = {}
+    for coo in totalCoo:
+        neighborBugCount = 0
+        for pt in erisNeighbors(coo):
+            if pt not in totalCoo:
+                newGrid[pt] = False
+                continue
+            if totalCoo[pt]:
+                neighborBugCount += 1
+        if totalCoo[coo]:  # tests if the bug has died or not
+            if not neighborBugCount == 1:
+                newGrid[coo] = False
+        else:  # calculates if the space should be infected or not
+            if neighborBugCount in [1, 2]:
+                newGrid[coo] = True
+    totalCoo.update(newGrid)
+
+bugCount = 0
+for coo in totalCoo:
+    if totalCoo[coo]:
+        bugCount += 1
+print("this shouldn't even work in actual science: %i" % bugCount)

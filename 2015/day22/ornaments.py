eaaf2a33f162d4ad20a1c1fd7d92439e93a865a3
Kevin Sheng <kevinsheng2005@gmail.com>
2020-03-31 11:15:54 -0700

    FINISHED IT YES

--- 2015/day22/ornaments.py
+++ 2015/day22/ornaments.py
@@ -3,15 +3,14 @@ from copy import deepcopy
 
 bossHealth = 71
 bossDamage = 10
-# recharge 101, 5 turns, missile 4, shield 7 armor 6, poison 3 damage, 6 turns
 spellList = {'Missile': 53, 'Drain': 73, 'Shield': 113, 'Recharge': 229, 'Poison': 173}
 effects = {'Shield', 'Poison', 'Recharge'}
 
 class Player:
     def __init__(self):
         self.effects = []
-        self.health = 10
-        self.mana = 250
+        self.health = 50
+        self.mana = 500
         self.manaSpent = 0
         self.bossHealth = bossHealth
         self.castedSpells = []
@@ -60,52 +59,50 @@ class Player:
             return 'we did it yay', self.manaSpent
         return 'what'
 
-bossHealth = 13
-bossDamage = 8
-def simulate(hardMode):  # takes in a game and returns all possible outcomes
+def simulate(hardCore):  # takes in a game and returns all possible outcomes
     loScore = float('inf')
     allGames = [Player()]
-    myTurn = True
     while allGames:
-
         newGames = []
         for g in allGames:
-            if hardMode:
-                g.health -= 1
             g.applyEffects()
-            newG = deepcopy(g)
             if g.checkDead() == 'what':
-                if myTurn:
-                    for s in spellList:
-                        newG = deepcopy(g)
-                        if s in [x[0] for x in newG.effects] and s in effects or spellList[s] > g.mana:
-                            continue
-                        newG.cast(s)
-                        results = newG.checkDead()
-                        if results == 'what':
-                            newGames.append(newG)
-                        elif results == 'oh noes':
-                            continue
-                        elif results[0] == 'we did it yay' and results[1] < loScore:
-                            loScore = results[1]
-                else:
-                    newG.bossAttack()
+                if hardCore:
+                    g.health -= 1
+                for s in spellList:
+                    newG = deepcopy(g)
+                    if s in [x[0] for x in newG.effects] and s in effects or spellList[s] > g.mana:
+                        continue
+                    newG.cast(s)
+                    results = newG.checkDead()
+                    if results == 'what':  # check if the player won or smth
+                        if newG.manaSpent > loScore:
+                            continue  # it's hopeless... well never get lower mana than this
+                    elif results == 'oh noes':
+                        continue
+                    elif results[0] == 'we did it yay' and results[1] < loScore:
+                        loScore = results[1]
+
+                    newG.applyEffects()  # maybe the effects will kill the boss
+                    if newG.checkDead()[0] == 'we did it yay' and newG.checkDead()[1] < loScore:
+                        loScore = newG.checkDead()[1]
+                        continue
+
+                    newG.bossAttack()  # well, boss's turn this time
                     results = newG.checkDead()
                     if results == 'what':
-                        newGames.append(newG)
+                        newGames.append(newG)  # onto the next round!
                     elif results == 'oh noes':
                         continue  # there's no way the player can win right after boss's turn right
 
-            elif newG.checkDead() == 'oh noes':
+            elif g.checkDead() == 'oh noes':  # probably will never come into effect but eh
                 continue
 
-            elif newG.checkDead()[0] == 'we did it yay' and newG.checkDead()[1] < loScore:  # if boss gets killed
-                loScore = newG.checkDead()[1]  # by a casted effect
+            elif g.checkDead()[0] == 'we did it yay' and g.checkDead()[1] < loScore:  # if boss gets killed by effects
+                loScore = g.checkDead()[1]  # by a casted effect
         allGames = newGames
-        print(len(allGames), myTurn, '-' * 40)
-        for g in allGames:
-            print(g.health, g.mana, g.effects, g.bossHealth, g.castedSpells)
-        myTurn = not myTurn
     return loScore
 
-print(simulate(False))
+print('FINALLY THIS TOOK ME SO FRICKIN LONG: %i' % simulate(False))
+print('but at least part two wasnt that hard to implement: %i' % simulate(True))
+print('and its slow but whatever')

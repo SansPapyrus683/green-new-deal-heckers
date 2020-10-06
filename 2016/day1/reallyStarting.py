d2cab634cabc4a45ed4483a5ccdc0284142e050f
SansPapyrus683 <kevinsheng2005@gmail.com>
2020-10-05 19:31:13 -0700

    ok finished 2016 day 1

--- 2016/day1/reallyStarting.py
+++ 2016/day1/reallyStarting.py
@@ -1,46 +1,43 @@
 import os
+os.chdir("2016/day1")
 
 with open('doLookLikeChristmas.txt') as read:
     directions = read.read().rstrip().split(sep=', ')
 
-def move(coo, facing, distance):
-    if facing == 'north':
-        coo[1] += distance
-
-    elif facing == 'south':
-        coo[1] -= distance
-
-    elif facing == 'east':
-        coo[0] += distance
-
-    elif facing == 'west':
-        coo[0] -= distance
-
+visitedBefore = {(0, 0)}
+gottenTwice = False
 coordinates = [0, 0]
-visitedBefore = [coordinates]
 currOrientation = 'north'
 rightPaths = {'north': 'east', 'east': 'south', 'south': 'west', 'west': 'north'}
 leftPaths = {'north': 'west', 'west': 'south', 'south': 'east', 'east': 'north'}
 
-first = True
+
+def move(coo, facing, distance):
+    global gottenTwice
+    if facing == 'north':
+        action = "coo[1] += 1"
+    elif facing == 'south':
+        action = "coo[1] -= 1"
+    elif facing == 'east':
+        action = "coo[0] += 1"
+    elif facing == 'west':
+        action = "coo[0] -= 1"
+
+    for _ in range(distance):
+        exec(action)
+        if tuple(coo) in visitedBefore and not gottenTwice:
+            print('bruh the protagonist is so dumb lol: %s' % sum([abs(i) for i in coo]))
+            gottenTwice = True
+        visitedBefore.add(tuple(coo))
+
+
+
 for d in directions:
-    prevCoo = coordinates.copy()  # TODO: this thing still hasn't been finished
     if d[0] == 'R':
         currOrientation = rightPaths[currOrientation]
         move(coordinates, currOrientation, int(d[1:]))
-        if coordinates[0] != prevCoo[0]:
-            pass  # move on x
-        else:
-            pass  # move on y
-
     else:
         currOrientation = leftPaths[currOrientation]
         move(coordinates, currOrientation, int(d[1:]))
-        if coordinates[0] != prevCoo[0]:
-            pass  # move on x
-        else:
-            pass  # move on y
 
-    visitedBefore.append(coordinates)
-
-print('what kind of stupid directions are these: %i' % abs(sum(coordinates)))  # part 1
+print('what kind of stupid directions are these: %i' % sum([abs(i) for i in coordinates]))  # part 1
